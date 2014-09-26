977a8f6a918a8ede3204abcacbcecefc245f3c4a
oldgeezr <chriram@dmlab06.(none)>
2014-09-26 11:32:26 +0200

    change control

--- exercise1/MIPSProcessor.vhd
+++ exercise1/MIPSProcessor.vhd
@@ -31,8 +31,72 @@ end MIPSProcessor;
 
 architecture DummyArch of MIPSProcessor is
 	signal counterReg : unsigned(31 downto 0);
+	
+	-- Jump/Branch mux out
+	signal next_pc_instruction : std_logic_vector(31 downto 0);
+	signal branch_instruction : std_logic_vector(31 downto 0);
+	signal pc_pluss_one : std_logic_vector(31 downto 0);
+	-- signal pc_source : std_logic_vector(1 downto 0);
+	signal branch : std_logic;
+	signal jump : std_logic;
+	signal branch_or_pc_pluss_one : std_logic_vector(31 downto 0);
+	
+	-- Control Unit Signals
+	signal clk : std_logic;
+	signal reset : std_logic;
+	signal opcode : std_logic_vector(5 downto 0);
+	signal mem_read : std_logic;
+	signal mem_write : std_logic;
+	signal mem_to_reg : std_logic;
+	signal reg_dest : std_logic;
+	signal reg_write : std_logic;
+	signal alu_op : std_logic_vector(1 downto 0);
+	signal alu_src : std_logic;
+	signal branch : std_logic;
+	signal jump : std_logic;	
+	
+	-- Control Unit
+	component control is
+		port (	-- Input
+					clk : in std_logic;
+					reset : in std_logic;
+					opcode : in std_logic_vector(5 downto 0);
+					-- Output 
+					mem_read : out std_logic;
+					mem_write : out std_logic;
+					mem_to_reg : out std_logic;
+					reg_dest : out std_logic;
+					reg_write : out std_logic;
+					alu_op : out std_logic_vector(1 downto 0);
+					alu_src: out std_logic;
+					branch: out std_logic;
+					jump: out std_logic
+					);	
+	end component;
+	
 begin
 
+	-- Initialize the Control Unit
+	control_unit: control port map (	clk => clk,
+												reset => reset,
+												opcode => opcode,
+												mem_read => mem_read,
+												mem_write => mem_write,
+												mem_to_reg => mem_to_reg,
+												ir_write => ir_write,
+												reg_dest => reg_dest,
+												reg_write => reg_write,
+												alu_op => alu_op,
+												alu_src => alu_src,
+												branch => branch,
+												jump => jump);
+	
+	-- Branch MUX
+	branch_or_pluss_one <= pc_pluss_one when branch = '0' else 
+	
+	-- Jump MUX
+	next_pc_source <= 
+										
 	DummyProc: process(clk, reset)
 	begin
 		if reset = '1' then
