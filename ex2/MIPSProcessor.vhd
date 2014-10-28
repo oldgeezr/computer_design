4fb540792583d4f2a938d98c1fa818f3e68dcd50
oldgeezr <ramstadevensen@hotmail.com>
2014-10-28 18:14:45 +0100

    new stage registers

--- ex2/MIPSProcessor.vhd
+++ ex2/MIPSProcessor.vhd
@@ -32,38 +32,38 @@ end MIPSProcessor;
 
 architecture Behavioral of MIPSProcessor is
 
- -- ---------------------------------
- -- -- Register Declarations
- -- ---------------------------------
+  ---------------------------------
+  -- Register Declarations
+  ---------------------------------
 
- -- -- Instruction Fetch/Decode
- -- signal if_id_new_pc                 : std_logic_vector(ADDR_WIDTH-1 downto 0);
- -- signal if_id_opcode                 : std_logic_vector(5 downto 0);
- -- signal if_id_rs                     : std_logic_vector(REG_WIDTH-1 downto 0);
- -- signal if_id_rt                     : std_logic_vector(REG_WIDTH-1 downto 0);
- -- signal if_id_rd                     : std_logic_vector(REG_WIDTH-1 downto 0);
- -- signal if_id_address                : std_logic_vector(15 downto 0);
+  -- Instruction Fetch/Decode
+  signal if_id_new_pc                 : std_logic_vector(ADDR_WIDTH-1 downto 0);
+  signal if_id_opcode                 : std_logic_vector(5 downto 0);
+  signal if_id_rs                     : std_logic_vector(REG_WIDTH-1 downto 0);
+  signal if_id_rt                     : std_logic_vector(REG_WIDTH-1 downto 0);
+  signal if_id_rd                     : std_logic_vector(REG_WIDTH-1 downto 0);
+  signal if_id_address                : std_logic_vector(15 downto 0);
 
- -- -- Instruction Decode/Execute
- -- signal id_ex_read_data_1            : std_logic_vector(DATA_WIDTH-1 downto 0);
- -- signal id_ex_read_data_2            : std_logic_vector(DATA_WIDTH-1 downto 0);
- -- signal id_ex_sign_extend            : std_logic_vector(DATA_WIDTH-1 downto 0);
- -- signal id_ex_rs                     : std_logic_vector(REG_WIDTH-1 downto 0);
- -- signal id_ex_rt                     : std_logic_vector(REG_WIDTH-1 downto 0);
- -- signal id_ex_rd                     : std_logic_vector(REG_WIDTH-1 downto 0);
- -- signal id_ex_address                : std_logic_vector(15 downto 0);
+  -- Instruction Decode/Execute
+  signal id_ex_read_data_1            : std_logic_vector(DATA_WIDTH-1 downto 0);
+  signal id_ex_read_data_2            : std_logic_vector(DATA_WIDTH-1 downto 0);
+  signal id_ex_sign_extend            : std_logic_vector(DATA_WIDTH-1 downto 0);
+  signal id_ex_rs                     : std_logic_vector(REG_WIDTH-1 downto 0);
+  signal id_ex_rt                     : std_logic_vector(REG_WIDTH-1 downto 0);
+  signal id_ex_rd                     : std_logic_vector(REG_WIDTH-1 downto 0);
+  signal id_ex_address                : std_logic_vector(15 downto 0);
 
- -- -- Execute/Memory
- -- signal ex_mem_alu_result            : std_logic_vector(DATA_WIDTH-1 downto 0);
- -- -- signal ex_mem_branch_addr           : std_logic_vector(ADDR_WIDTH-1 downto 0);
- -- signal ex_mem_rd                    : std_logic_vector(REG_WIDTH-1 downto 0);
- -- signal ex_mem_address               : std_logic_vector(15 downto 0);
+  -- Execute/Memory
+  signal ex_mem_alu_result            : std_logic_vector(DATA_WIDTH-1 downto 0);
+  -- signal ex_mem_branch_addr           : std_logic_vector(ADDR_WIDTH-1 downto 0);
+  signal ex_mem_rd                    : std_logic_vector(REG_WIDTH-1 downto 0);
+  signal ex_mem_address               : std_logic_vector(15 downto 0);
 
- -- -- Memory/Writeback
- -- signal mem_wb_read_dmem             : std_logic_vector(DATA_WIDTH-1 downto 0);
- -- signal mem_wb_alu_result            : std_logic_vector(DATA_WIDTH-1 downto 0);
- -- signal mem_wb_rd                    : std_logic_vector(REG_WIDTH-1 downto 0);
- -- signal mem_wb_address               : std_logic_vector(15 downto 0);
+  -- Memory/Writeback
+  signal mem_wb_read_dmem             : std_logic_vector(DATA_WIDTH-1 downto 0);
+  signal mem_wb_alu_result            : std_logic_vector(DATA_WIDTH-1 downto 0);
+  signal mem_wb_rd                    : std_logic_vector(REG_WIDTH-1 downto 0);
+  signal mem_wb_address               : std_logic_vector(15 downto 0);
 
   ---------------------------------
   -- Signal Declarations
@@ -226,18 +226,18 @@ begin
     forward_a               => forward_a,
     forward_b               => forward_b);
 
-  stage_regs : entity work.stage_registers(rtl) port map (
-    clk                     => clk,
-    reset                   => reset,
-    new_pc                  => new_pc,
-    opcode                  => opcode,
-    if_address              => if_address,
-    read_data_1             => read_data_1,
-    read_data_2             => read_data_2,
-    sign_extend             => sign_extend,
-    alu_result              => alu_result,
-    ex_rd                   => ex_rd,
-    dmem_data_in            => dmem_data_in);
+  --stage_regs : entity work.stage_registers(rtl) port map (
+  --  clk                     => clk,
+  --  reset                   => reset,
+  --  new_pc                  => new_pc,
+  --  opcode                  => opcode,
+  --  if_address              => if_address,
+  --  read_data_1             => read_data_1,
+  --  read_data_2             => read_data_2,
+  --  sign_extend             => sign_extend,
+  --  alu_result              => alu_result,
+  --  ex_rd                   => ex_rd,
+  --  dmem_data_in            => dmem_data_in);
 
   -- *************************** --
   -- Main Curcuit
@@ -324,63 +324,63 @@ begin
   ---------------------------------
   -- Stage Registers
   ---------------------------------
-  -- stage_registers : process ( clk, reset,
-  --                             new_pc,
-  --                             opcode,
-  --                             if_rs,
-  --                             if_rt,
-  --                             if_rd,
-  --                             if_address,
-  --                             read_data_1,
-  --                             read_data_2,
-  --                             sign_extend,
-  --                             if_id_rs,
-  --                             if_id_rt,
-  --                             if_id_rd,
-  --                             if_id_address,
-  --                             alu_result,
-  --                             ex_rd,
-  --                             id_ex_address,
-  --                             dmem_data_in,
-  --                             ex_mem_alu_result,
-  --                             ex_mem_rd,
-  --                             ex_mem_address
-  --   )
-  -- begin
-  --   if reset = '1' then
-  --     -- Do the reset thingy
-  --   else
-  --     if (rising_edge(clk)) then
-  --       -- Instruction Fetch/Decode
-  --       if_id_new_pc      <= new_pc;
-  --       if_id_opcode      <= opcode;
-  --       if_id_rs          <= if_rs;
-  --       if_id_rt          <= if_rt;
-  --       if_id_rd          <= if_rd;
-  --       if_id_address     <= if_address;
+  stage_registers : process ( clk, reset,
+                              new_pc,
+                              opcode,
+                              if_rs,
+                              if_rt,
+                              if_rd,
+                              if_address,
+                              read_data_1,
+                              read_data_2,
+                              sign_extend,
+                              if_id_rs,
+                              if_id_rt,
+                              if_id_rd,
+                              if_id_address,
+                              alu_result,
+                              ex_rd,
+                              id_ex_address,
+                              dmem_data_in,
+                              ex_mem_alu_result,
+                              ex_mem_rd,
+                              ex_mem_address
+    )
+  begin
+    if reset = '1' then
+      -- Do the reset thingy
+    else
+      if (rising_edge(clk)) then
+        -- Instruction Fetch/Decode
+        if_id_new_pc      <= new_pc;
+        if_id_opcode      <= opcode;
+        if_id_rs          <= if_rs;
+        if_id_rt          <= if_rt;
+        if_id_rd          <= if_rd;
+        if_id_address     <= if_address;
 
-  --       -- Instruction Decode/Execute
-  --       id_ex_read_data_1 <= read_data_1;
-  --       id_ex_read_data_2 <= read_data_2;
-  --       id_ex_sign_extend <= sign_extend;
-  --       id_ex_rs          <= if_id_rs;
-  --       id_ex_rt          <= if_id_rt;
-  --       id_ex_rd          <= if_id_rd;
-  --       id_ex_address     <= if_id_address;
+        -- Instruction Decode/Execute
+        id_ex_read_data_1 <= read_data_1;
+        id_ex_read_data_2 <= read_data_2;
+        id_ex_sign_extend <= sign_extend;
+        id_ex_rs          <= if_id_rs;
+        id_ex_rt          <= if_id_rt;
+        id_ex_rd          <= if_id_rd;
+        id_ex_address     <= if_id_address;
 
-  --       -- Execute/Memory
-  --       ex_mem_alu_result <= alu_result;
-  --       ex_mem_rd         <= ex_rd; -- from the mux here
-  --       ex_mem_address    <= id_ex_address;
+        -- Execute/Memory
+        ex_mem_alu_result <= alu_result;
+        ex_mem_rd         <= ex_rd; -- from the mux here
+        ex_mem_address    <= id_ex_address;
 
-  --       -- Memory/Write Back
-  --       mem_wb_read_dmem  <= dmem_data_in;
-  --       mem_wb_alu_result <= ex_mem_alu_result;
-  --       mem_wb_rd         <= ex_mem_rd;
-  --       mem_wb_address    <= ex_mem_address;
-  --     end if;
-  --   end if;
-  -- end process;
+        -- Memory/Write Back
+        mem_wb_read_dmem  <= dmem_data_in;
+        mem_wb_alu_result <= ex_mem_alu_result;
+        mem_wb_rd         <= ex_mem_rd;
+        mem_wb_address    <= ex_mem_address;
+      end if;
+    end if;
+  end process;
 
 end Behavioral;
 
