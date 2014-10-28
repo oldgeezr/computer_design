08c65520cc44f855c8bd45add39b47cc96818c6f
oldgeezr <ramstadevensen@hotmail.com>
2014-10-28 18:04:19 +0100

    new stage registers

--- ex2/stage_registers.vhd
+++ ex2/stage_registers.vhd
@@ -5,8 +5,8 @@ entity stage_registers is
   generic (
     ADDR_WIDTH          : integer := 8;
     DATA_WIDTH          : integer := 32;
-    REG_WIDTH           : integer := 5;
-  )
+    REG_WIDTH           : integer := 5
+  );
   port (
     clk                 : in std_logic;
     reset               : in std_logic;
@@ -34,6 +34,40 @@ entity stage_registers is
 end entity;
 
 architecture rtl of stage_registers is
+
+  ---------------------------------
+  -- Register Declarations
+  ---------------------------------
+
+  -- Instruction Fetch/Decode
+  signal if_id_new_pc                 : std_logic_vector(ADDR_WIDTH-1 downto 0);
+  signal if_id_opcode                 : std_logic_vector(5 downto 0);
+  signal if_id_rs                     : std_logic_vector(REG_WIDTH-1 downto 0);
+  signal if_id_rt                     : std_logic_vector(REG_WIDTH-1 downto 0);
+  signal if_id_rd                     : std_logic_vector(REG_WIDTH-1 downto 0);
+  signal if_id_address                : std_logic_vector(15 downto 0);
+
+  -- Instruction Decode/Execute
+  signal id_ex_read_data_1            : std_logic_vector(DATA_WIDTH-1 downto 0);
+  signal id_ex_read_data_2            : std_logic_vector(DATA_WIDTH-1 downto 0);
+  signal id_ex_sign_extend            : std_logic_vector(DATA_WIDTH-1 downto 0);
+  signal id_ex_rs                     : std_logic_vector(REG_WIDTH-1 downto 0);
+  signal id_ex_rt                     : std_logic_vector(REG_WIDTH-1 downto 0);
+  signal id_ex_rd                     : std_logic_vector(REG_WIDTH-1 downto 0);
+  signal id_ex_address                : std_logic_vector(15 downto 0);
+
+  -- Execute/Memory
+  signal ex_mem_alu_result            : std_logic_vector(DATA_WIDTH-1 downto 0);
+  -- signal ex_mem_branch_addr           : std_logic_vector(ADDR_WIDTH-1 downto 0);
+  signal ex_mem_rd                    : std_logic_vector(REG_WIDTH-1 downto 0);
+  signal ex_mem_address               : std_logic_vector(15 downto 0);
+
+  -- Memory/Writeback
+  signal mem_wb_read_dmem             : std_logic_vector(DATA_WIDTH-1 downto 0);
+  signal mem_wb_alu_result            : std_logic_vector(DATA_WIDTH-1 downto 0);
+  signal mem_wb_rd                    : std_logic_vector(REG_WIDTH-1 downto 0);
+  signal mem_wb_address               : std_logic_vector(15 downto 0);
+
 begin
 
   stage_registers : process (
